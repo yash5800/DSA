eac19a0c271fc4a67d4b2a8ab164662ca2facd9f
A.Yaswanth Kiran <113412043+yash5800@users.noreply.github.com>
2025-01-17 14:19:17 +0530

    day 5 learning DSA

--- LinkedList.js
+++ LinkedList.js
@@ -14,19 +14,14 @@ class ll{
   
   //PUSH Operation 
   push(val){
+    let newnode = new node(val)
     if(this.head.next === null){
-        let newnode = new node(val)
         this.head.next = newnode
         this.tail = newnode
         this.length++
     }
     else{
-      let pre = this.head
-      while(pre.next!==null){
-        pre = pre.next
-      }
-      let newnode = new node(val)
-      pre.next = newnode
+      this.tail.next = newnode
       this.tail = newnode
       this.length++
     }
@@ -44,6 +39,7 @@ class ll{
       this.tail = pre2
       this.tail.next = null
       this.length--
+      pre2.next = null
     }
     else{
       console.log("list is empty man...")
@@ -61,6 +57,95 @@ class ll{
     pre.next = newnode
     this.length++
   }
+
+  //SHIFT Operation
+  shift(){
+    let pre = this.head.next
+    if(pre!==null){
+      this.head.next = pre.next
+      this.length--
+      pre.next = null
+    }
+    else{
+      console.log("list is empty man...")
+    }
+  }
+
+  //GETFIRST Operation
+  getFirst(){
+    if(!this.head.next){
+      console.log("list is empty man...")
+    }
+    else{
+      return this.head.next
+    }
+  }
+
+  //GETLAST Operation
+  getLast(){
+    if(typeof(this.tail.data) == 'string'){
+      console.log("list is empty man...")
+    }
+    else{
+      return this.tail
+    }
+  }
+
+  //GET Operation
+  get(index){
+     if(this.length == 0) console.log("list is empty man...")
+     else if(this.length - 1 > index && index >= 0 ){
+       let pre = this.head.next
+       for(let i = 0; i < index; i++){
+          pre = pre.next
+       }
+       return pre  
+     }
+     else console.log("Index out view...")
+  }
+
+  //SET Operation like updating
+  set(index,val){
+    if(this.length == 0) console.log("list is empty man...")
+    else if(this.length - 1 > index && index >= 0 ){
+      let pre = this.get(index)
+      pre.data = val
+      return pre
+    }
+    else console.log("Index out view...")
+  }
+
+  //INSERT Operation 
+  insert(pos,val){
+    let newnode = new node(val)
+
+    if(pos === 's') this.unshift(val)
+    else if(pos === 'e') this.push(val)
+    else{
+      if(pos === 0) this.unshift(val)
+      else if(pos > this.length - 1) console.log("Index out view...")
+      else{
+         let pre = this.get(pos-1)
+         newnode.next = pre.next
+         pre.next = newnode
+         this.length++
+         pre=null
+      }
+    }
+  }
+
+  //SIZE Operations
+  size(){
+    return this.length
+  }
+
+  //CLEAR Operations
+  clear(){
+    this.head.next = null
+    this.tail = this.head
+    this.length = 0
+  }
+
 }
 
 const myll = new ll()
@@ -71,4 +156,10 @@ myll.pop()
 myll.unshift(10)
 myll.push(3)
 myll.unshift(5)
+myll.shift()
+console.log(myll.set(0,3))
+myll.insert(3,1)
+console.log(myll.size())
+myll.clear()
+
 console.log(myll)
